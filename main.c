8f1272ea9194a46f19c24bb8c749875675891979
Keegan <kcm507@mun.ca>
2018-12-14 16:37:02 -0330

    More progress still buggy

--- main.c
+++ main.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include<time.h>
+#include <time.h>
 
 int board[7][6] = {0};
 
+/*int board[7][6] = {
+    {1,2,1,1,1,1},
+    {1,2,1,2,1,1},
+    {1,1,2,1,1,1},
+    {1,2,2,2,1,1},
+    {2,1,2,2,1,1},
+    {2,2,1,2,2,2},
+    {2,1,2,2,2,2}
+};*/
 
 struct position
 {
@@ -63,106 +72,212 @@ bool isConnectFour(struct position pos)
     int id = board[pos.x][pos.y];
     //printf("%d\n",id);
     //printf("%d,%d\n",pos.x,pos.y);
+
     int count = 0;
+
+    int boundX1 = 0;
+    int boundX2 = 6;
+    int boundY1 = 0;
+    int boundY2 = 5;
+
     if (pos.y > 2)
     {
         for (int i = 1; i < 4; i++)
         {
-            
-            if (id != board[pos.x][pos.y - i])
+            if (id == board[pos.x][pos.y - i])
             {
-                break;
+                count++;
+                if (count == 3)
+                {
+                    found = true;
+                    printf("Vertical Connect\n");
+                    return found;
+                }
             }
             else
             {
-                found = true;
-                printf("vertical connect\n");
-                return found;
+                count = 0;
+                break;
             }
         }
     }
     for (int i = -3; i < 4; i++)
     {
-        //printf("%d,%d\n", id, board[pos.x + i][pos.y]);
-        if (id == board[pos.x + i][pos.y])
+        //printf("%d,%d\n", pos.x, pos.x + i);
+        if (pos.x + i >= boundX1 && pos.x + i <= boundX2)
         {
-            //printf("incrementing \n");
-            count++;
-            if (count == 4)
+            //printf("past bound 1\n");
+            //printf("%d,%d\n", id, board[pos.x + i][pos.y]);
+            if (id == board[pos.x + i][pos.y])
             {
-                found = true;
-                printf("horizontal connect\n");
-                return found;
+                //printf("incrementing \n");
+                //printf("count up\n");
+                count++;
+                if (count == 4)
+                {
+                    found = true;
+                    printf("horizontal connect\n");
+                    return found;
+                }
+            }
+            else
+            {
+                //printf("count reset\n");
+                count = 0;
             }
         }
-        else
-        {
-            count = 0;
-        }
     }
+    //printf("end of drop\n");
     count = 0;
     for (int i = -3; i < 4; i++)
     {
-
-        if (id == board[pos.x + i][pos.y + i])
+        if (pos.y + i >= boundY1 && pos.y + i <= boundY2)
         {
-            count++;
-            if (count == 4)
+            if (pos.x + i >= boundX1 && pos.x + i <= boundX2)
             {
-                found = true;
-                printf("diangle 1 connect\n");
-                return found;
+                if (id == board[pos.x + i][pos.y + i])
+                {
+                    count++;
+                    //printf("%d,", count);
+                    if (count == 4)
+                    {
+                        found = true;
+                        printf("diangle 1 connect\n");
+                        return found;
+                    }
+                }
+                else
+                {
+                    count = 0;
+                }
             }
         }
-        else
-        {
-            count = 0;
-        }
     }
+
     count = 0;
     for (int i = -3; i < 4; i++)
     {
-
-        if (id == board[pos.x + i][pos.y - i])
+        if (pos.y - i >= boundY1 && pos.y - i <= boundY2)
         {
-            count++;
-            if (count == 4)
+            if (pos.x + i >= boundX1 && pos.x + i <= boundX2)
             {
-                found = true;
-                printf("diangle 2 connect\n");
-                return found;
+                if (id == board[pos.x + i][pos.y - i])
+                {
+                    count++;
+                    if (count == 4)
+                    {
+                        found = true;
+                        printf("diangle 2 connect\n");
+                        return found;
+                    }
+                }
+                else
+                {
+                    count = 0;
+                }
             }
-        }
-        else
-        {
-            count = 0;
         }
     }
     return found;
 }
 
+bool validDrop(int col)
+{
+    bool result = true;
+    if (board[col][5] != 0)
+    {
+        result = false;
+    }
+    return result;
+}
+
+int KeeganAI()
+{
+    int id = 2;
+    int oppID = 1;
+    bool found = false;
+    int choice = 0;
+    struct position pos;
+    for (int i = 0; i < 7; i++)
+    {
+        if (validDrop(i))
+        {
+            pos = dropPiece(id, i);
+            found = isConnectFour(pos);
+            board[pos.x][pos.y] = 0;
+            if (found)
+            {
+                choice = i;
+                return choice;
+            }
+        }
+    }
+    for (int i = 0; i < 7; i++)
+    {
+        if (validDrop(i))
+        {
+            pos = dropPiece(oppID, i);
+            found = isConnectFour(pos);
+            board[pos.x][pos.y] = 0;
+            if (found)
+            {
+                choice = i;
+                return choice;
+            }
+        }
+    }
+    do{
+        choice = rand() % 5 + 1;
+    }while(!validDrop);
+    
+    return choice;
+}
+
 int main()
 {
     srand(time(NULL));
-    for (int i = 0; i < 3; i++)
+
+    bool gameover = false;
+    struct position pos;
+    int id = 2;
+    int shifter = -1;
+    int col = 0;
+    int sentinel = 1;
+    int turn = 0;
+    while (sentinel)
     {
         bool gameover = false;
-        struct position pos;
-        int id = 1;
-        int turn = 0;
-        int shifter = 1;
-        while (!gameover && turn < 42)
+        while (!gameover)
         {
-            
-            pos = dropPiece(id, (rand() % 6));
-            gameover = isConnectFour(pos);
             turn++;
             id = id + shifter;
             shifter *= -1;
+            if (turn % 2 == 1)
+            {
+                do
+                {
+                    printf("choose a VALID column {0,1,2,3,4,5,6}: ");
+                    scanf("%d", &col);
+                    printf("WTF");
+                } while (!validDrop(col));
+            }
+            else
+            {
+                col = KeeganAI();
+            }
+            pos = dropPiece(id, col);
+            printBoard();
+            printf("\n");
+            gameover = isConnectFour(pos);
+            if (turn >= 42)
+            {
+                gameover = true;
+            }
         }
-        printBoard();
-        printf("%d wins! \n", board[pos.x][pos.y]);
         reset();
+        turn = 0;
+        printf("would you like to play again? {0,1}");
+        scanf("%d", &sentinel);
     }
 
     return 0;
